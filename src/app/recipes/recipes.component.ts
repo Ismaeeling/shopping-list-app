aeec9ba357b2b7e860a31c19bf357c430ba6a730
Ismaeel <e30.sa23@gmial.com>
2018-10-15 13:10:34 +0800

    Recipe Services bug fixed

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { RecipeServices } from './recipe.service';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css'],
-  providers:[RecipeServices]
+  styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
   constructor() { }
